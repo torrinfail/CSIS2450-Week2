32d0cfc352c20ae3426d0dcbc9b2f30f622a5ed6
Aidan <torrinfail@gmail.com>
2021-01-30 15:16:01 -0800

    Finished assignment

--- Week2.java
+++ Week2.java
@@ -1,22 +1,53 @@
+/*
+ * Aidan Hubert CSIS 2450 Week 2
+ */
+
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 public class Week2
 {
-	static double[][] temperatures;
+	//constants
 	static final String CSV_PATH = "SLCDecember2020Temperatures.csv";
+	static final String REPORT_PATH = "TemperaturesReport.txt";
 	static final String LINE = "--------------------------------------------------------------\n";
+	static final String SCALE_LABEL =
+			LINE+
+			"      1   5    10   15   20   25   30   35   40   45   50\n"+
+			"      |   |    |    |    |    |    |    |    |    |    |\n"+
+			LINE;
+	static final int N_DAYS = 31;
+	
+	
 	static int highestDay, lowestDay;
-	static double  highestTemp, lowestTemp = Double.MAX_VALUE;
+	static double  highTemps, highestTemp, lowTemps,lowestTemp = Double.MAX_VALUE;
+	static double[][] temperatures;
 
 	public static void main(String[] args)
 	{
-		temperatures = new double[31][3];
-		FileReader csvFile = null;
+		temperatures = new double[N_DAYS][3];
+
 		StringBuilder sb = new StringBuilder();
 		try
 		{
-			csvFile = new FileReader(CSV_PATH);
+			readTemps(sb);
+			sb.setLength(0);
+			createReport(sb);
+			System.out.print(sb);
+			writeReport(sb);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			//System.out.println("Error: " + e.getMessage());
+		}
+	}
+	
+	static void readTemps(StringBuilder sb) throws IOException
+	{
+		try (FileReader csvFile = new FileReader(CSV_PATH))
+		{
 			int c = 0, i = 0, j = 0; 
 			while ((c = csvFile.read()) != -1)
 			{
@@ -30,6 +61,8 @@ public class Week2
 				{
 					temperatures[i][j] = Double.parseDouble(sb.toString());
 					sb.setLength(0);
+					highTemps += temperatures[i][1];
+					lowTemps += temperatures[i][2];
 					i++;
 					j = 0;
 					continue;
@@ -37,14 +70,16 @@ public class Week2
 				sb.append((char)c);
 			}
 		}
-		catch (IOException e){}
-		sb.setLength(0);
-
-		printAndStore(LINE, sb);
-		printAndStore("December 2020: Temperatures in Utah\n", sb);
-		printAndStore(LINE, sb);
-		printAndStore("Day\tHigh\tLow\tVariance\n", sb);
-		printAndStore(LINE, sb);
+	}
+	
+	//create report and store it in sb
+	static void createReport(StringBuilder sb)
+	{
+		sb.append(LINE);
+		sb.append("December 2020: Temperatures in Utah\n");
+		sb.append(LINE);
+		sb.append("Day\tHigh\tLow\tVariance\n");
+		sb.append(LINE);
 
 
 		for (int i = 0; i < temperatures.length; i++)
@@ -61,44 +96,53 @@ public class Week2
 			}
 			for (int j = 0; j < temperatures[i].length; j++)
 			{
-				printAndStore(Double.toString(temperatures[i][j]), sb);
+				//append day, high, low depending on value of j
+				sb.append(String.format("%.0f", temperatures[i][j]));
 				if (j < temperatures[i].length-1)
-					printAndStore("\t", sb);
+					sb.append("\t");
 				else
 				{
-					printAndStore("\t", sb);
-					printAndStore(Double.toString(temperatures[i][j-1] - temperatures[i][j]), sb);
+					//append variance
+					sb.append("\t");
+					sb.append(String.format("%.0f", temperatures[i][j-1] - temperatures[i][j]));
 				}
 			}
-			printAndStore("\n", sb);
+			sb.append("\n");
 		}
-		printAndStore("December Highest Temperature: 12/" +
+		sb.append("December Highest Temperature: 12/" +
 				temperatures[highestDay][0] + 
 				": " + highestTemp + " Average Hi: " +
-				(temperatures[highestDay][1] + temperatures[highestDay][2])/2.0 + "\n", sb);
+				String.format("%.1f", highTemps/N_DAYS) + "\n");
 
-		printAndStore("December Lowest Temperature: 12/" +
+		sb.append("December Lowest Temperature: 12/" +
 				temperatures[lowestDay][0] + 
 				": " + lowestTemp + " Average Lo: " +
-				(temperatures[lowestDay][1] + temperatures[lowestDay][2])/2.0 + "\n", sb);
+				String.format("%.1f", lowTemps/N_DAYS) + "\n");
+		sb.append(LINE);
+		sb.append("Graph\n");
+		sb.append(SCALE_LABEL);
 		for (int i = 0; i < temperatures.length; i++)
 		{
-			printAndStore(Double.toString(temperatures[i][0]) + " Hi " + getRepeatedCharString('+', (int)temperatures[i][1]) + "\n",sb);
-			printAndStore("   Lo " + getRepeatedCharString('-', (int)temperatures[i][2]) + "\n",sb);
+			sb.append(String.format("%-2.0f", temperatures[i][0]) + " Hi " + getRepeatedCharString('+', (int)temperatures[i][1]) + "\n");
+			sb.append("   Lo " + getRepeatedCharString('-', (int)temperatures[i][2]) + "\n");
 		}
-
+		sb.append(SCALE_LABEL);
+	}
+	
+	//write report to report file
+	static void writeReport(StringBuilder sb) throws IOException
+	{
+		FileWriter writer = new FileWriter(new File(REPORT_PATH));
+		writer.append(sb);
+		writer.close();
 	}
 
+	//returns a string that contains the same character repeated nChars times
 	static String getRepeatedCharString(char c, int nChars)
 	{
 		char[] chars = new char[nChars];
 		for (int i = 0; i < chars.length; i++)
 			chars[i] = c;
 		return new String(chars);
-	}
-	static void printAndStore(String str, StringBuilder sb)
-	{
-		sb.append(str);
-		System.out.print(str);
 	}
 }
